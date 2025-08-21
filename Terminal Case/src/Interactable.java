8b3304ddf15e8f093d3bd498715e8a277a49ca39
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-08-20 21:50:40 -0300

    comments

--- Terminal Case/src/Interactable.java
+++ Terminal Case/src/Interactable.java
@@ -1,4 +1,7 @@
-public class Interactable
+// still have to think more about it, but I think everything will be an Interactable, from people to objects
+// Then they override interact(), or something like that
+
+public abstract class Interactable
 {
     private String name;
 
