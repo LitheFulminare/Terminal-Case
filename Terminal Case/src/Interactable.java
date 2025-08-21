1be7fed31938c27de9b06849ec263547acf27432
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-08-20 21:33:31 -0300

    init & setup classes

---
+++ Terminal Case/src/Interactable.java
@@ -0,0 +1,14 @@
+public class Interactable
+{
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ Terminal Case/src/Inventory.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory
+{
+    List<Item> items = new ArrayList<>();
+}
