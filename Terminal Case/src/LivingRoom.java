689565cb3f59ee20d8785e2904ad0ca04869bfec
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-11-12 02:49:22 -0300

    setup new room

---
+++ Terminal Case/src/LivingRoom.java
@@ -0,0 +1,13 @@
+import events.InputEvent;
+import events.PrintMessageEvent;
+import publisherSubscriber.GameEvent;
+
+
+public class LivingRoom extends AbstractRoom
+{
+    @Override
+    public void enter()
+    {
+        System.out.println("hmm coisas");
+    }
+}
---
+++ Terminal Case/src/events/GoToLivingRoomEvent.java
@@ -0,0 +1,11 @@
+package events;
+
+import publisherSubscriber.GameEvent;
+
+public class GoToLivingRoomEvent implements GameEvent
+{
+    public GoToLivingRoomEvent()
+    {
+
+    }
+}
